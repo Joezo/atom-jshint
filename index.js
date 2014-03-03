8bb7c072d2742353dbda520fd82ddcab0892f9a0
Joezo <joe.warren@holidayextras.com>
2014-03-03 20:25:15 +0000

    Start of error panel, need to look into unsubscribing from cursor:moved event only

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var AtomJsHint = require('./lib/atom-jshint.js');
 module.exports = {
   configDefaults: {
     hintOnSave: true,
-    hintOnModify: true
+    hintOnModify: true,
+    showErrorPanel: true
   },
   activate: function(){
     return this.atomJsHint = new AtomJsHint();
